cc0c9fd33253803e5364512827700b9eee5c2b2f
zakelguindi <89213113+zakelguindi@users.noreply.github.com>
2023-11-30 19:35:07 -0500

    hope it works

--- server.js
+++ server.js
@@ -32,8 +32,6 @@ const teamSchema = new mongoose.Schema({
 
 const Team = mongoose.model("Team", teamSchema); 
 
-// let nbateams = [
-
 
 app.get("/api/nbateams", (req, res) => {
   getTeams(res); 
